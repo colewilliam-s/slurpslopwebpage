02ee4d90a23107587bebeeec8402f93291016a72
colewilliam-s <colewilliams0614@gmail.com>
2025-11-22 01:10:20 -0500

    getting rid of my attempt at the slider for right now, just going to try to make an actual usable website. just gotta remember that this doesnt need to be perfect initially, just needs to direct people to the chrome extension

--- src/slider.js
+++
@@ -1,5 +0,0 @@
-let slider = document.getElementById('slider');
-let topImage = document.getElementById('top');
-slider.addEventListener('input', () => {
-    topImage.style.clipPath = `inset(0 ${100 - slider.value}% 0 0)`;
-})
